1df3af53de60df73d1abeea489c79f19f1d0b012
iilincca <ilinca.georgescu@icloud.com>
2025-05-19 03:14:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -188,3 +188,12 @@ function determinaGrupa() {
     explicatie.textContent = detalii;
   }
 }
+
+const btn = document.getElementById('darkModeBtn');
+
+btn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  btn.textContent = document.body.classList.contains('dark-mode')
+    ? 'Dezactivează modul întunecat'
+    : 'Activează modul întunecat';
+});
